d0d4fa404eb17cba3d8c39724393c2ace1bde706
Deepanshu Kumar <iamdeepanshu9@gmail.com>
2026-01-06 13:29:58 +0530

    fixed middleware error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,43 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'admin.deepanshukumar.com',
+          },
+        ],
+        destination: '/admin/:path*',
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'admin.localhost(:\\d+)?',
+          },
+        ],
+        destination: '/admin/:path*',
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/admin/:path*',
+        missing: [
+          { type: 'host', value: 'admin.deepanshukumar.com' },
+          { type: 'host', value: 'admin.localhost(:\\d+)?' }
+        ],
+        destination: '/404',
+        permanent: false,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
